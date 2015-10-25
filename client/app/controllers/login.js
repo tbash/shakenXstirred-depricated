bf022330f9726a2604b6e8688d3469828647ae19
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-10-25 16:04:29 -0400

    Signup auto authenticates and handles some validation before hitting the server

--- client/app/controllers/login.js
+++ client/app/controllers/login.js
@@ -5,8 +5,13 @@ export default Ember.Controller.extend({
 
   actions: {
     authenticate() {
+      var nameRegex = /^[a-z0-9_-]{1,16}$/;
+      var passwordRegex = /^[a-z0-9_-]{8,18}$/;
+      
       const { name, password } = this.getProperties('name', 'password');
-      return this.get('session').authenticate('authenticator:custom', name, password);
+      if (nameRegex.test(name) && nameRegex.test(password)) {
+        return this.get('session').authenticate('authenticator:custom', name, password);
+      }
     }
   }
 });
--- client/app/controllers/signup.js
+++ client/app/controllers/signup.js
@@ -1,18 +1,35 @@
 import Ember from 'ember';
  
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  
   actions: {
     create: function() {
+      var nameRegex = /^[a-z0-9_-]{1,16}$/;
+      var passwordRegex = /^[a-z0-9_-]{8,18}$/;
       var name = this.get('name');
       var password = this.get('password');
-      var password_confirmation = this.get('password_confirmation');                         
-      var user = this.store.createRecord('user', {
-        name: name,
-        password: password,
-        password_confirmation: password_confirmation
-      });
+      var password_confirmation = this.get('password_confirmation');
+      if (nameRegex.test(name) === false) {
 
-      user.save();
+      }
+      else if (password !== password_confirmation === false) {
+
+      }
+      else if (passwordRegex.test(password)) {
+        
+      }
+      else {
+        var user = this.store.createRecord('user', {
+          name: name,
+          password: password,
+          password_confirmation: password_confirmation
+        });
+        var self = this;
+        user.save().then(function() {
+          self.get('session').authenticate('authenticator:custom', name, password);
+        });
+      }
     }
   }
 });
