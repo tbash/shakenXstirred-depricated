83e4d4c531400bf299e77189de2845d2ff0f5751
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-10-30 18:45:03 -0400

    Removing unused js

--- client/app/intializers/session-store.js
+++
@@ -1,9 +0,0 @@
-export function initialize(container, application) {
-  application.inject('session:custom', '_store', 'store:main')
-}
-
-export default {
-  name: 'session-store',
-  after: 'ember-data',
-  initialize: initialize
-}
