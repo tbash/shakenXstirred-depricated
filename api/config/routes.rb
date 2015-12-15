21a3d10a4112349eefb3148d521a06f9fae86122
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-12-14 23:16:51 -0500

    Drops unnecessary routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   post 'users/:id/update_inventory',    to: 'users#update_inventory'
   post '/session',                      to: 'sessions#create'
 
-
   resources :ingredients
-  get '/ingredients/:id/cocktails',     to: 'ingredients#show_cocktails'
-
   resources :cocktails
-  get '/cocktails/:id/ingredients',     to: 'cocktails#show_ingredients'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
