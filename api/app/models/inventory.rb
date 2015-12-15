33ef525533e9834ae9baaa5bfb92030067f32f94
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-12-14 20:29:51 -0500

    moves update inventory into model and removes controller

--- api/app/models/inventory.rb
+++ api/app/models/inventory.rb
@@ -1,4 +1,19 @@
 class Inventory < ActiveRecord::Base
   belongs_to :user
   belongs_to :ingredient
+  after_initialize :update_inventory
+
+  def update_inventory
+    user = User.find params[:id]
+    incoming_ingredients = Ingredient.find(params[:ingredients])
+    user.ingredients = (incoming_ingredients & user.ingredients) | incoming_ingredients
+    incoming_cocktails = []
+    Mixture.where(ingredient_id: params[:ingredient_ids]).each do |mix|
+      if (mix.cocktail.ingredients - user.ingredients).empty?
+        incoming_cocktails << mix.cocktail
+      end
+    end
+    
+    user.cocktails = (incoming_cocktails & user.cocktails) | incoming_cocktails
+  end
 end
