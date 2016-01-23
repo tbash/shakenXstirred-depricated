f3cef7f5d5b0f12699c293eee27f19eb1f22ea98
T. Brandon Ashley <tbash@knights.ucf.edu>
2016-01-23 15:19:01 -0500

    removes unused shell installs

--- tba.sh
+++ tba.sh
@@ -20,10 +20,7 @@ echo installing Bundler
 gem install bundler -N >/dev/null 2>&1
 
 install Git git
-install SQLite sqlite3 libsqlite3-dev
-install memcached memcached
 install Redis redis-server
-install RabbitMQ rabbitmq-server
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
