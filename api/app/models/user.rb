261b1806ad4d3360f9fdd882932d5a38104b5453
T. Brandon Ashley <tbash@knights.ucf.edu>
2016-01-25 23:46:42 -0500

    Corrects update_cocktails method

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -5,19 +5,22 @@ class User < ApplicationRecord
   has_many :ingredients, through: :inventories
   has_secure_password
 
-  def update_mixologies(ingredient_ids)
-    Mixture.where(ingredient_id: ingredient_ids).each do |mix|
-      if (mix.cocktail.ingredients - self.ingredients).empty?
-        cocktails << mix.cocktail
+  def update_cocktails(ingredient_ids)
+    cocktail_ids = Mixture.where(ingredient_id: ingredient_ids).pluck(:cocktail_id).uniq
+    cocktails = Cocktail.where(id: cocktail_ids)
+    can_make = []
+    cocktails.each do |cocktail|
+      if (cocktail.ingredients - self.ingredients).empty?
+        can_make << cocktail
       end
     end
 
-    self.cocktails = cocktails
+    self.cocktails = can_make
   end
 
   def update_inventory(ingredient_ids)
     ingredients = Ingredient.find(ingredient_ids)
     self.ingredients = ingredients
-    self.update_mixologies ingredient_ids
+    self.update_cocktails ingredient_ids
   end
 end
